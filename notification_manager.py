1ada0be34955f02eabfdcbfb131ff9a8081e1e5c
Mustafa Ali Khan <khan49605@gmail.com>
2024-07-27 05:21:59 +0530

    Initial commit

--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,23 @@
+import os
+from twilio.rest import Client
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.client = Client(os.environ['TWILIO_SID'], os.environ["TWILIO_AUTH_TOKEN"])
+
+    def send_sms(self, message_body):
+
+        message = self.client.messages.create(
+            from_=os.environ["TWILIO_VIRTUAL_NUMBER"],
+            body=message_body,
+            to=os.environ["NUMBER"]
+        )
+
+        print(message.sid)
+
+    def send_whatsapp(self, message_body):
+        message = self.client.messages.create(
+            from_=f'whatsapp:{os.environ["TWILIO_WHATSAPP_NUMBER"]}',
+            body=message_body,
+            to=f'whatsapp:{os.environ["NUMBER"]}'
+        )
+        print(message.sid)
